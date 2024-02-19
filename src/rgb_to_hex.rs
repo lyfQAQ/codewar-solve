6565754be8951759563feb257233b87a1425da3c
lyfQAQ <0219lyf@gmail.com>
2024-02-19 20:42:48 +0800

    reduce rgb to hex

--- src/rgb_to_hex.rs
+++ src/rgb_to_hex.rs
@@ -1,18 +1,10 @@
 fn rgb(r: i32, g: i32, b: i32) -> String {
-    let nums = vec![r, g, b];
-    let nums: Vec<_> = nums
-        .into_iter()
-        .map(|x| {
-            if x < 0 {
-                0
-            } else if x > 255 {
-                255
-            } else {
-                x
-            }
-        })
-        .collect();
-    format!("{:02X}{:02X}{:02X}", nums[0], nums[1], nums[2])
+    format!(
+        "{:02X}{:02X}{:02X}",
+        r.clamp(0, 255),
+        g.clamp(0, 255),
+        b.clamp(0, 255)
+    )
 }
 
 #[test]
